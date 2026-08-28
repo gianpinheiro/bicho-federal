fd42e86ed96314fa48f3401063caeaf7e7446f21
gianpinheiro <gianpinheiro15@gmail.com>
2026-08-28 10:58:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-import streamlit as st
-import requests
+import streamlit as st, requests
 URL = st.secrets["TURSO_URL"].rstrip("/") + "/v2/pipeline"
 TOKEN = st.secrets["TURSO_TOKEN"]
 HEADERS = {"Authorization": f"Bearer {TOKEN}", "Content-Type": "application/json"}
 
-payload = {"requests": [{"type":"execute","stmt":{"sql": "SELECT name FROM sqlite_master WHERE type='table'"}}]}
-r = requests.post(URL, headers=HEADERS, json=payload).json()
-st.json(r)
+def consulta(sql):
+    payload = {"requests": [{"type":"execute","stmt":{"sql": sql}}]}
+    r = requests.post(URL, headers=HEADERS, json=payload).json()
+    st.code(sql)
+    st.json(r)
+
+consulta("SELECT * FROM banca_sorteios LIMIT 3")
+consulta("SELECT * FROM bicho LIMIT 5")
